105fb086302718ade9efa2cbff3880185d97b176
2029-td <cstd22099@g.nihon-u.ac.jp>
2025-10-10 03:03:31 +0900

    完了と未完了の入れ替え

--- src/features/todo/components/TodoDetailModal.tsx
+++ src/features/todo/components/TodoDetailModal.tsx
@@ -100,7 +100,7 @@ const TodoDetailModal: React.FC<TodoDetailModalProps> = ({
             buttonColor={colors.surface}
             textColor={theme.textColor}
           >
-            {localCompleted ? '未完了' : '完了'}
+            {localCompleted ? '完了' : '未完了'}
           </Button>
 
           <Button
